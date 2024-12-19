d66904d2ac8cdafa047aa3ea889e50df3c19576b
Syed Aqeel Ashiq <madnaelo@yahoo.com>
2024-12-19 13:06:23 +0400

    Update image_converter.js

--- image_converter.js
+++ image_converter.js
@@ -52,7 +52,7 @@ app.get("/process-image", async (req, res) => {
       imageBuffer = await sharp(buffer)
         .resize(maxDim, maxDim, {
           fit: "contain",
-          background: { r: 255, g: 255, b: 255, a: 1 }, // Transparent padding
+          background: { r: 240, g: 240, b: 240, a: 0 }, // Transparent padding
         })
         .toBuffer();
      const newMetaData = await sharp(imageBuffer).metadata();

52256a208fcf2845894ec1ee6d0c7de04a36d9a6
Syed Aqeel Ashiq <madnaelo@yahoo.com>
2024-12-19 12:35:29 +0400

    Update image_converter.js

--- image_converter.js
+++ image_converter.js
@@ -57,6 +57,9 @@ app.get("/process-image", async (req, res) => {
           background: { r: 255, g: 255, b: 255, alpha: 0 }, // Transparent padding
         })
         .toBuffer();
+     const newMetaData = await sharp(buffer).metadata();
+     console.log("Original dimension" + metadata.width + " X " + metadata.height); 
+     console.log("new dimension" + newMetaData.width + " X " + newMetaData.height); 
     }
 
     // Return the image
